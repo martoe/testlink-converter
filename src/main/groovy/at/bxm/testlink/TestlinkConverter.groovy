d322aca2e7b7c8deada7613b5661d4d3a853d492
Martin Ehrnhoefer <martin@ehrnhoefer.com>
2016-08-25 13:28:33 +0200

    First test

--- src/main/groovy/at/bxm/testlink/TestlinkConverter.groovy
+++ src/main/groovy/at/bxm/testlink/TestlinkConverter.groovy
@@ -20,10 +20,33 @@ class TestlinkConverter {
 
   String processSheet(Sheet sheet) {
     def writer = new StringWriter()
-      def xml = new MarkupBuilder(writer)
-    xml.root {
-      for (int i = 0; i < 10; i++) {
-        blah(x: i)
+    def xml = new MarkupBuilder(writer)
+    xml.testcases {
+      testcase(internalid: "6", name: "Test Case") {
+        node_order(1000)
+        version(1)
+        summary("Test Case Summary")
+        preconditions()
+        execution_type(1)
+        importance(2)
+        estimated_exec_duration {}
+        status(1)
+        is_open(1)
+        active(1)
+        steps {
+          step {
+            step_number(1)
+            actions("<p> step 1 </p>")
+            expectedresults("<p> result 1 </p>")
+            execution_type(1)
+          }
+          step {
+            step_number(2)
+            actions("<p> step 2 </p>")
+            expectedresults("<p> result 2 </p>")
+            execution_type(1)
+          }
+        }
       }
     }
     return writer as String
--- src/test/groovy/at/bxm/testlink/TestlinkConverterTest.groovy
+++ src/test/groovy/at/bxm/testlink/TestlinkConverterTest.groovy
@@ -2,10 +2,41 @@ package at.bxm.testlink
 
 import org.junit.Test
 
+import static org.hamcrest.CoreMatchers.equalTo
+import static org.hamcrest.MatcherAssert.assertThat
+
 class TestlinkConverterTest {
 
   @Test
   void test() {
-    println new TestlinkConverter().convert("src/test/resources/empty.xlsx" as File)
+    def converted = new TestlinkConverter().convert("src/test/resources/empty.xlsx" as File)
+    assertThat(converted, equalTo("""<testcases>
+  <testcase internalid='6' name='Test Case'>
+    <node_order>1000</node_order>
+    <version>1</version>
+    <summary>Test Case Summary</summary>
+    <preconditions />
+    <execution_type>1</execution_type>
+    <importance>2</importance>
+    <estimated_exec_duration />
+    <status>1</status>
+    <is_open>1</is_open>
+    <active>1</active>
+    <steps>
+      <step>
+        <step_number>1</step_number>
+        <actions>&lt;p&gt; step 1 &lt;/p&gt;</actions>
+        <expectedresults>&lt;p&gt; result 1 &lt;/p&gt;</expectedresults>
+        <execution_type>1</execution_type>
+      </step>
+      <step>
+        <step_number>2</step_number>
+        <actions>&lt;p&gt; step 2 &lt;/p&gt;</actions>
+        <expectedresults>&lt;p&gt; result 2 &lt;/p&gt;</expectedresults>
+        <execution_type>1</execution_type>
+      </step>
+    </steps>
+  </testcase>
+</testcases>"""))
   }
 }
