e6ef156bc491e9f80c8de19a5f88690ee022ce10
Martin Ehrnhoefer <martin@ehrnhoefer.com>
2016-08-25 14:37:24 +0200

    Basic implementation (WIP)

--- src/main/groovy/at/bxm/testlink/TestlinkConverter.groovy
+++ src/main/groovy/at/bxm/testlink/TestlinkConverter.groovy
@@ -5,6 +5,17 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException
 import org.apache.poi.ss.usermodel.Sheet
 import org.apache.poi.ss.usermodel.WorkbookFactory
 
+import javax.xml.transform.stream.StreamSource
+import javax.xml.validation.SchemaFactory
+
+import static java.lang.Thread.currentThread
+import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI
+
+/**
+ * Documentation: https://github.com/TestLinkOpenSourceTRMS/testlink-documentation/tree/master/end-users/1.9
+ * A Ecxel-XML converter: https://github.com/MrBricodage/TestLink--ImportExcelMacro
+ *
+ */
 class TestlinkConverter {
 
   String convert(File excelFile) {
@@ -21,34 +32,54 @@ class TestlinkConverter {
   String processSheet(Sheet sheet) {
     def writer = new StringWriter()
     def xml = new MarkupBuilder(writer)
+    def counter = 0
     xml.testcases {
-      testcase(internalid: "6", name: "Test Case") {
-        node_order(1000)
-        version(1)
-        summary("Test Case Summary")
-        preconditions()
-        execution_type(1)
-        importance(2)
-        estimated_exec_duration {}
-        status(1)
-        is_open(1)
-        active(1)
-        steps {
-          step {
-            step_number(1)
-            actions("<p> step 1 </p>")
-            expectedresults("<p> result 1 </p>")
+
+      sheet.rowIterator().each { row ->
+        String testId = row.getCell(0)
+        if (testId && testId != "TF_ID") { // skip header lines
+          String testGoal = row.getCell(1)
+          String testPrecond = row.getCell(2)
+          def testSteps = (row.getCell(3) as String).readLines()
+          String testExpectation = row.getCell(4)
+          String testData = row.getCell(5)
+          //...
+
+          testcase(internalid: counter++, name: testId) {
+            node_order(1000)
+            version(1)
+            summary(testGoal)
+            preconditions(testPrecond)
             execution_type(1)
-          }
-          step {
-            step_number(2)
-            actions("<p> step 2 </p>")
-            expectedresults("<p> result 2 </p>")
-            execution_type(1)
+            importance(2)
+            estimated_exec_duration {}
+            status(1)
+            is_open(1)
+            active(1)
+            steps {
+              testSteps.eachWithIndex { String testStep, int testStepNo ->
+                step {
+                  step_number(testStepNo + 1)
+                  actions(testStep)
+                  if (testStepNo == testSteps.size() - 1) {
+                    expectedresults(testExpectation)
+                  }
+                  execution_type(1)
+                }
+              }
+            }
           }
         }
       }
     }
     return writer as String
+  }
+
+  static void validate(String xml) {
+    URL schema = currentThread().getContextClassLoader().getResource("testcases.xsd");
+    SchemaFactory.newInstance(W3C_XML_SCHEMA_NS_URI)
+      .newSchema(schema)
+      .newValidator()
+      .validate(new StreamSource(new StringReader(xml)))
   }
 }
--- src/test/groovy/at/bxm/testlink/TestlinkConverterTest.groovy
+++ src/test/groovy/at/bxm/testlink/TestlinkConverterTest.groovy
@@ -8,14 +8,14 @@ import static org.hamcrest.MatcherAssert.assertThat
 class TestlinkConverterTest {
 
   @Test
-  void test() {
-    def converted = new TestlinkConverter().convert("src/test/resources/empty.xlsx" as File)
+  void "convert simple file"() {
+    def converted = new TestlinkConverter().convert("src/test/resources/simple-testcase.xlsx" as File)
     assertThat(converted, equalTo("""<testcases>
-  <testcase internalid='6' name='Test Case'>
+  <testcase internalid='0' name='F-001'>
     <node_order>1000</node_order>
     <version>1</version>
     <summary>Test Case Summary</summary>
-    <preconditions />
+    <preconditions>Preconditions</preconditions>
     <execution_type>1</execution_type>
     <importance>2</importance>
     <estimated_exec_duration />
@@ -25,18 +25,28 @@ class TestlinkConverterTest {
     <steps>
       <step>
         <step_number>1</step_number>
-        <actions>&lt;p&gt; step 1 &lt;/p&gt;</actions>
-        <expectedresults>&lt;p&gt; result 1 &lt;/p&gt;</expectedresults>
+        <actions>1. Do first</actions>
         <execution_type>1</execution_type>
       </step>
       <step>
         <step_number>2</step_number>
-        <actions>&lt;p&gt; step 2 &lt;/p&gt;</actions>
-        <expectedresults>&lt;p&gt; result 2 &lt;/p&gt;</expectedresults>
+        <actions>2. Do next</actions>
+        <execution_type>1</execution_type>
+      </step>
+      <step>
+        <step_number>3</step_number>
+        <actions>3. Do last</actions>
+        <expectedresults>Expected result</expectedresults>
         <execution_type>1</execution_type>
       </step>
     </steps>
   </testcase>
 </testcases>"""))
+  }
+
+  @Test
+  void "convert real data"() {
+    def file = "Regressionsliste_FICO_Kommentare Christoph_überarbeitet"
+    new File("target/${file}.xml").text = new TestlinkConverter().convert(new File("src/test/resources/${file}.xlsx"))
   }
 }
