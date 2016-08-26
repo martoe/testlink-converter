4a6057fd5b778d7ca6ccc74f6d105a87a282e773
Martin Ehrnhoefer <martin@ehrnhoefer.com>
2016-08-26 09:43:16 +0200

    New Excel template

--- src/main/groovy/at/bxm/testlink/TestlinkConverter.groovy
+++ src/main/groovy/at/bxm/testlink/TestlinkConverter.groovy
@@ -1,7 +1,9 @@
 package at.bxm.testlink
 
+import groovy.util.logging.Slf4j
 import groovy.xml.MarkupBuilder
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException
+import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.ss.usermodel.Sheet
 import org.apache.poi.ss.usermodel.WorkbookFactory
 
@@ -13,12 +15,19 @@ import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI
 
 /**
  * Documentation: https://github.com/TestLinkOpenSourceTRMS/testlink-documentation/tree/master/end-users/1.9
- * A Ecxel-XML converter: https://github.com/MrBricodage/TestLink--ImportExcelMacro
+ * A Excel-XML converter: https://github.com/MrBricodage/TestLink--ImportExcelMacro
  *
  */
+@Slf4j
 class TestlinkConverter {
 
+  def idCounter
+  def xml
+
   String convert(File excelFile) {
+    idCounter = 0
+    def xmlWriter = new StringWriter()
+    xml = new MarkupBuilder(xmlWriter)
     if (!excelFile.exists() || !excelFile.canRead() || !excelFile.file) {
       throw new FileNotFoundException("Cannot read local file $excelFile.absolutePath")
     }
@@ -26,53 +35,65 @@ class TestlinkConverter {
     if (wb.numberOfSheets > 1 && wb.activeSheetIndex != 0) {
       throw new InvalidFormatException("File contains multiple sheets - which one to use?")
     }
-    return processSheet(wb.getSheetAt(0))
+    processSheet(wb.getSheetAt(0))
+    return xmlWriter.toString()
   }
 
-  String processSheet(Sheet sheet) {
-    def writer = new StringWriter()
-    def xml = new MarkupBuilder(writer)
-    def counter = 0
+  private void processSheet(Sheet sheet) {
     xml.testcases {
-
+      def testCaseRows = []
       sheet.rowIterator().each { row ->
         String testId = row.getCell(0)
-        if (testId && testId != "TF_ID") { // skip header lines
+        if (testId != "TF_ID") { // skip header line
+          if (testId) { // start of a new testcase block
+            processTestcase(testCaseRows)
+            testCaseRows.clear()
+          }
           String testGoal = row.getCell(1)
-          String testPrecond = row.getCell(2)
-          def testSteps = (row.getCell(3) as String).readLines()
-          String testExpectation = row.getCell(4)
-          String testData = row.getCell(5)
-          //...
+          String testAction = row.getCell(4)
+          if (testGoal || testAction) {
+            testCaseRows.add(row)
+          } else {
+            println("Skipping empty row " + (row.rowNum + 1))
+          }
+        }
+      }
+      processTestcase(testCaseRows)
+    }
+  }
 
-          testcase(internalid: counter++, name: testId) {
-            node_order(1000)
-            version(1)
-            summary(testGoal)
-            preconditions(testPrecond)
-            execution_type(1)
-            importance(2)
-            estimated_exec_duration {}
-            status(1)
-            is_open(1)
-            active(1)
-            steps {
-              testSteps.eachWithIndex { String testStep, int testStepNo ->
-                step {
-                  step_number(testStepNo + 1)
-                  actions(testStep)
-                  if (testStepNo == testSteps.size() - 1) {
-                    expectedresults(testExpectation)
-                  }
-                  execution_type(1)
-                }
-              }
+  private void processTestcase(List<Row> rows) {
+    if (rows) {
+      String testId = rows[0].getCell(0)
+      String testGoal = rows[0].getCell(1)
+      String testPrecond = rows[0].getCell(2)
+      // ...
+      xml.testcase(internalid: idCounter++, name: testId) {
+        node_order(1000)
+        version(1)
+        summary(testGoal)
+        preconditions(testPrecond)
+        execution_type(1)
+        importance(2)
+        estimated_exec_duration {}
+        status(1)
+        is_open(1)
+        active(1)
+        steps {
+          rows.eachWithIndex { row, testStepNo ->
+            String testStepAction = row.getCell(4)
+            String testStepDesc = row.getCell(5)
+            String testStepResult = row.getCell(6)
+            step {
+              step_number(testStepNo + 1)
+              actions("$testStepAction <br/> $testStepDesc")
+              expectedresults(testStepResult)
+              execution_type(1)
             }
           }
         }
       }
     }
-    return writer as String
   }
 
   static void validate(String xml) {
--- src/test/groovy/at/bxm/testlink/TestlinkConverterTest.groovy
+++ src/test/groovy/at/bxm/testlink/TestlinkConverterTest.groovy
@@ -11,7 +11,7 @@ class TestlinkConverterTest {
   void "convert simple file"() {
     def converted = new TestlinkConverter().convert("src/test/resources/simple-testcase.xlsx" as File)
     assertThat(converted, equalTo("""<testcases>
-  <testcase internalid='0' name='F-001'>
+  <testcase internalid='0' name='T-001'>
     <node_order>1000</node_order>
     <version>1</version>
     <summary>Test Case Summary</summary>
@@ -25,17 +25,19 @@ class TestlinkConverterTest {
     <steps>
       <step>
         <step_number>1</step_number>
-        <actions>1. Do first</actions>
+        <actions>OL Login &lt;br/&gt; Do first</actions>
+        <expectedresults></expectedresults>
         <execution_type>1</execution_type>
       </step>
       <step>
         <step_number>2</step_number>
-        <actions>2. Do next</actions>
+        <actions>Menü Suche &lt;br/&gt; Do next</actions>
+        <expectedresults></expectedresults>
         <execution_type>1</execution_type>
       </step>
       <step>
         <step_number>3</step_number>
-        <actions>3. Do last</actions>
+        <actions>Postkorbeintrag auswählen &lt;br/&gt; Do last</actions>
         <expectedresults>Expected result</expectedresults>
         <execution_type>1</execution_type>
       </step>
@@ -46,7 +48,7 @@ class TestlinkConverterTest {
 
   @Test
   void "convert real data"() {
-    def file = "Regressionsliste_FICO_Kommentare Christoph_überarbeitet"
+    def file = "Regressionsliste_neu_Template"
     new File("target/${file}.xml").text = new TestlinkConverter().convert(new File("src/test/resources/${file}.xlsx"))
   }
 }
